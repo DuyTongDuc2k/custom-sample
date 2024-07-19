fa9d2286d9575307f46c419810d7a82b597441fc
giryu <giryu@ubuntu.myguest.virtualbox.org>
2024-07-19 13:43:27 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ export WORKPLACE=`pwd`
 PATCH="image-patch.ipp.yml"
 CUSTOM="custom.ipp.yml"
 
+
 # apply the patch and then create the custom distro yml
 
 # Step 1: Get the original distro yml.
@@ -22,12 +23,13 @@ ORIGIN_DISTRO=$(yq ".distro" $PATCH | sed 's/"//g') # f40, cs9, eln, f38
 
 ORIGIN_DISTRO=$(find $DISTRO_FOLDER -name "$ORIGIN_DISTRO.ipp.yml") || echo "$ORIGIN_DISTRO.ipp.yml not found"
 
+APPLY_PATCH_PATH=$(find $DATA -name "apply-patch.py")
 
 # Apply the patch to the origin distro and then create the custom distro
 rm -rf $CUSTOM
 touch $CUSTOM
 
-python3 apply-patch.py $ORIGIN_DISTRO $PATCH -o $CUSTOM
+python3 $APPLY_PATCH_PATH $ORIGIN_DISTRO $PATCH -o $CUSTOM
 
 # Copy the custom distro to distro folder
 
@@ -39,9 +41,9 @@ echo "Start the build for the custom distro"
 
 sleep 3
 
-make custom-qemu-developer-regular.aarch64.qcow2
+#make custom-qemu-developer-regular.aarch64.qcow2
 
-mv custom-qemu-developer-regular.aarch64.qcow2 $WORKPLACE
+#mv custom-qemu-developer-regular.aarch64.qcow2 $WORKPLACE
 
 rm -rf $DISTRO_FOLDER/$CUSTOM
 
